cc25cbfed7295b324658db54ad4e366c03f0b9bd
juan buelvas <juandbuelvas@unicesar.edu.co>
2026-03-10 21:24:08 -0500

    feat: crear clase buseta

--- src/transcesar/model/Buseta.java
+++ src/transcesar/model/Buseta.java
@@ -8,6 +8,17 @@ package transcesar.model;
  *
  * @author Pulgarin
  */
-public class Buseta {
-    
+public class Buseta extends Vehiculo {
+    public Buseta(String placa, String ruta) {
+        super(placa, ruta);
+        this.capacidadMaxima = 19;
+        this.tarifaBase = 8000;
+    }
+
+    @Override
+    public void imprimirDetalle() {
+        System.out.println("BUSETA | Placa: " + placa + " | Ruta: " + ruta +
+            " | Cupos: " + (capacidadMaxima - pasajerosActuales) + 
+            " | Tarifa: $" + tarifaBase);
+    }
 }
