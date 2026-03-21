1d47b49a55e254765b4df7765a7ccdc6680a7b51
juan buelvas <juandbuelvas@unicesar.edu.co>
2026-03-21 11:39:38 -0500

    feat: crear estructura base de ReservaDAO

---
+++ src/transcesar/dao/ReservaDAO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package transcesar.dao;
+
+
+import transcesar.model.Reserva;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReservaDAO {
+    private static final String ARCHIVO_RESERVAS = "reservas.txt";
+}
