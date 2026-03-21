05aec7f17ea7ee6f9049442992c0142a0ec75924
roberto buelvas <rcarlosbuelvas@unicesar.edu.co>
2026-03-21 11:46:28 -0500

    feat:agegue las reservas en memoriaaaa

--- src/transcesar/service/ReservaService.java
+++ src/transcesar/service/ReservaService.java
@@ -29,3 +29,25 @@ public class ReservaService {
         verificarReservasVencidas();
     }
 }
+
+  private void cargarReservasEnMemoria() {
+        List<String[]> datos = reservaDAO.cargarDatosReservas();
+        for (int i = 0; i < datos.size(); i++) {
+            String[] fila = datos.get(i);
+            try {
+                String codigo = fila[0];
+                Pasajero pasajero = personaService.buscarPasajeroPorCedula(fila[1]);
+                Vehiculo vehiculo = vehiculoService.buscarVehiculoPorPlaca(fila[2]);
+                String fechaCreacion = fila[3];
+                String fechaViaje = fila[4];
+                String estado = fila[5];
+                if (pasajero != null && vehiculo != null) {
+                    Reserva r = new Reserva(codigo, pasajero, vehiculo, fechaCreacion, fechaViaje);
+                    r.setEstado(estado);
+                    reservas.add(r);
+                }
+            } catch (Exception e) {
+                System.out.println("Advertencia: no se pudo cargar una reserva.");
+            }
+        }
+    }
